f75a91244591d4cbd014734c57ca90e1f9463463
Amit Dhakal <amit.dhakal423@gmail.com>
2024-02-26 11:21:45 +0545

    test file

---
+++ src/main/java/com/example/JenkinsTestFile/JenkinsTestFileApplication.java
@@ -0,0 +1,13 @@
+package com.example.JenkinsTestFile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsTestFileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsTestFileApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/JenkinsTestFile/JenkinsTestFileApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.JenkinsTestFile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsTestFileApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
