0f0e80b0c977775107196c02d90172146317b12b
Amit Dhakal <amit.dhakal423@gmail.com>
2024-02-26 13:26:32 +0545

    modify

--- src/main/java/com/example/JenkinsTestFile/controller/WebController.java
+++ src/main/java/com/example/JenkinsTestFile/controller/WebController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class WebController {
-@GetMapping("/")
+@GetMapping("/home")
     public String getHomePage(){
 
         return "index";
